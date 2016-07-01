6354569a76e7fdcb776738a32516e207edf7a934
Ovidiu-Florin BOGDAN <ovidiu.b13@gmail.com>
2016-07-01 14:55:19 +0300

    Added initial Dockerfile. Install ReviewBoard pipy.

---
+++ Dockerfile
@@ -0,0 +1,37 @@
+FROM httpd:2.4.20
+MAINTAINER Ovidiu-Florin Bogdan (ovidiu.b13@gmail.com)
+
+ENV DEBIAN_FRONTEND noninteractive
+
+RUN apt update && apt install -y \
+    patch \
+    subversion \
+    memcached \
+    ca-certificates
+RUN apt update && apt install -y \
+    python-setuptools \
+    python-dev \
+    python-mysqldb \
+    python-svn
+RUN apt install -y \
+    libjpeg-dev \
+    zlib1g-dev \
+    libffi-dev \
+    libssl-dev
+
+RUN easy_install -U setuptools
+RUN easy_install pip
+RUN pip install python-memcached
+RUN pip install mysql-python
+
+RUN apt-get install gcc -s | grep "Inst " | cut -f 2 -d " " > gcc-deps.txt
+RUN apt install -y gcc
+
+RUN pip install ReviewBoard
+
+RUN apt remove -y `cat gcc-deps.txt | tr "\n" " "` && rm gcc-deps.txt
+
+COPY entrypoint.sh /entrypoint.sh
+
+ENTRYPOINT ["/entrypoint.sh"]
+CMD ["mysql"]
---
+++ entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+# TODO Support for postgress
+
+if [[ "$1" == mysql ]]; then
+
+    : "${REVIEWBOARD_DB_HOSTNAME:=mysql}"
+    # if we're linked to MySQL and thus have credentials already, let's use them
+    : ${REVIEWBOARD_DB_USER:=${MYSQL_ENV_MYSQL_USER:-root}}
+    if [ "$REVIEWBOARD_DB_USER" = 'root' ]; then
+        : ${REVIEWBOARD_DB_PASSWORD:=$MYSQL_ENV_MYSQL_ROOT_PASSWORD}
+    fi
+    : ${REVIEWBOARD_DB_PASSWORD:=$MYSQL_ENV_MYSQL_PASSWORD}
+    : ${REVIEWBOARD_DB_NAME:=${MYSQL_ENV_MYSQL_DATABASE:-reviewboard}}
+
+    if [ -z "$REVIEWBOARD_DB_PASSWORD" ]; then
+        echo >&2 'error: missing required REVIEWBOARD_DB_PASSWORD environment variable'
+        echo >&2 '  Did you forget to -e REVIEWBOARD_DB_PASSWORD=... ?'
+        echo >&2
+        echo >&2 '  (Also of interest might be REVIEWBOARD_DB_USER and REVIEWBOARD_DB_NAME.)'
+        exit 1
+    fi
+
+    MYSQL_QUERRY="CREATE DATABASE IF NOT EXISTS $REVIEWBOARD_DB_NAME CHARACTER SET utf8;"
+
+fi
