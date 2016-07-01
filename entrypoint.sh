93e009ec70d35c243260c16e3121870e0a5250d2
Ovidiu-Florin BOGDAN <ovidiu.b13@gmail.com>
2016-07-01 16:11:25 +0300

    Added rb-site install command with DB and URL settings

--- entrypoint.sh
+++ entrypoint.sh
@@ -37,3 +37,21 @@ fi
 
 : "${REVIEWBOARD_DB_HOSTNAME:=dbserver}"
 : "${DOMAIN:=localhost}"
+
+mkdir -p /var/www/
+
+if [[ ! -d /var/www/reviewboard ]]; then
+# TODO Setup the Python loader with wsgi
+# TODO Add memcache
+    rb-site install --noinput \
+        --domain-name="$DOMAIN" \
+        --site-root=/ --static-url=static/ --media-url=media/ \
+        --db-type=$DATABASE \
+        --db-name="$REVIEWBOARD_DB_NAME" \
+        --db-host="$REVIEWBOARD_DB_HOSTNAME" \
+        --db-user="$REVIEWBOARD_DB_USER" \
+        --db-pass="$REVIEWBOARD_DB_PASSWORD" \
+        --web-server-type=apache --python-loader=modpython\
+        --admin-user=admin --admin-password=admin --admin-email=admin@example.com \
+        /var/www/reviewboard/
+fi
